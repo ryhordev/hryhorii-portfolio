0ca5e3d5f8dd5ba997c065a08e629ede2da9e22d
Hryhorii <hryhorii.petrenko@rocketta.de>
2024-03-26 09:59:57 +0100

    change full year logic

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,16 +1,24 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import me from "../../assets/me.jpg";
 
 import './index.css'
 
+const START_YEAR = 2022;
+
 export const Welcome: FC = () => {
+    const [expYears, setExpYears] = useState<number>();
+
+    useEffect(() => {
+        setExpYears(new Date().getFullYear() - START_YEAR)
+    }, []);
+
     return (
         <div className="main">
             <img src={me} className="main-me-image"></img>
             <h1 className="main-text">Hryhorii Petrenko</h1>
-            
+
             <h2>Full Stack Developer | .NET + React</h2>
-            <h2>Over 2 years of professional experience</h2>
+            <h2>Over {expYears} years of professional experience</h2>
 
             <button className="btn btn-primary main-button">Discover Projects & Articles</button>
         </div>
